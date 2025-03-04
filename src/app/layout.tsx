648a38dfd7bcc6245d201d7cfbdd9f4a720f6bb7
Andres Galaviz <5544243+andresgalaviz@users.noreply.github.com>
2025-03-04 12:36:57 -0500

    Added metadata and picture

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,22 +12,63 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: 'Andres Galaviz',
+  title: "Andres Galaviz",
+  description:
+    "Ex-Microsoft Senior Eng Manager - Azure Cloud Infra & Robotics.",
   icons: {
-    icon: '/favicon.ico'
+    icon: "/favicon.ico",
   },
-}
+  openGraph: {
+    type: "profile",
+    title: "Andres Galaviz - Personal Website",
+    description:
+      "Ex-Microsoft Senior Eng Manager - Azure Cloud Infra & Robotics.",
+    url: "https://galaviz.io",
+    images: [
+      "https://galaviz.io/AndresGalaviz.jpg",
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Andres Galaviz - Personal Website",
+    description:
+      "Ex-Microsoft Senior Eng Manager in Cloud Infra & Robotics. Master's in CS at UW.",
+    images: ["https://galaviz.io/AndresGalaviz.jpg"],
+    site: "@AndresGalaviz",
+  },
+};
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "Person",
+    name: "Andres Galaviz",
+    url: "https://galaviz.io",
+    jobTitle: "ex-Senior Software Engineering Manager",
+    worksFor: {
+      "@type": "Organization",
+      name: "Microsoft",
+    },
+    alumniOf: "University of Washington",
+    sameAs: [
+      "https://www.linkedin.com/in/andresgalaviz",
+      "https://github.com/andresgalaviz",
+    ],
+  };
+
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+      </head>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-// src/app/page.tsx
-
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-neutral-900 text-neutral-100">
